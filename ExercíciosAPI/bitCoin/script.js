a7a8921bbc17b76cde1191c52b3a090d1ea83a58
Luis Felipe Silva Bandeira <luisfelipe363732@gmail.com>
2022-09-16 17:23:59 -0300

    NewExercise

--- ExercíciosAPI/bitCoin/script.js
+++ ExercíciosAPI/bitCoin/script.js
@@ -27,6 +27,4 @@ async function consultaBitCoin(){
 }
 consultaBitCoin()
 
-// Utilizando a API https://api.chucknorris.io/jokes/random
-// retorne uma piada randomica do chucknorris, toda vez que
-// clicar em próxima
+
